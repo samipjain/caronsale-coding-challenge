5a2aaa06392a771169c695709c44866af6edc8fe
Samip Jain <samipjain1216@gmail.com>
2020-08-07 01:19:16 -0400

    Add Router, Login & Overview Page

--- challenges/web-frontend/caronsale-samip/src/app/app.module.ts
+++ challenges/web-frontend/caronsale-samip/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OverviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'overview', component: OverviewComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
